2befb0a1d50c99893d9df9f365ea921545c53c4f
Jay Windley <jwindley@xmission.com>
2019-10-29 13:22:06 -0600

    Convert XML parser/renderer to function-only interface

--- py/lib/MARC/XML.py
+++ py/lib/MARC/XML.py
@@ -1,93 +1,42 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -----------------------------------------------------------------------
-#  File:              TAG( MARC_XML.py )
+#  File:              TAG( XML.py )
 #  Description:       MARC XML parser and renderer
 #  Author:            Jay Windley <jwindley>
 #  Created:           Thu May  2 13:00:41 2013
 #  Copyright:         (c) 2013 Jay Windley
 #                     All rights reserved.
 # -----------------------------------------------------------------------
+"""Parse and/or render MARC records to/from XML"""
 
 import os
 import sys
 import xml.etree.ElementTree as ET
 
-from MARC import MARC_tag, MARC_record
-from MARC_bib import MARC_bib
+import MARC
 
-#-----------------------------------------------------------------------
-# A few decorators to properly qualify the element names in MARC-XML
-# elements.
-#
+# A few decorators to properly qualify the element names in MARC-XML elements.
 def tag( suffix ): return '{http://www.loc.gov/zing/srw/}' + suffix
 def ns( suffix ):  return '{http://www.loc.gov/MARC21/slim}' + suffix
 
 
-########################################################################
-#
-#  XML Parser
-#
-#  The parse_XXX() methods parse an XML file or a string,
-#  respectively, in the MARC XML format.
-#
-# XXX should probably inherit from something basic
-#
-class MARC_XML_parser( object ):
+def parse_tree( root: ET ) -> list:
+    """Return a list of MARC record objects contained in the ElementTree <root>.  Ostensibly this should
+    be only one, but this is meant for LC queries, and they can return multiple hits.  Returns empty
+    list if the file contains no MARC records.
 
-    #-------------------------------------------------------------------
-    # Parse <s> as XML and return the associated ElementTree, or None
-    # on failure.
+    """
+    records = []
+
+    if root is None: return records
+
+    # Parse a single XML element representing an entire MARC21 record and return a new MARC.record
+    # object embodying it.  Returns None on error.
     #
-    # XXX: should probably figure out how to merge with the file-based
-    # function, since the distinction for this purpose is silly.
-    #
-    def parse_string( self, s ):
-        tree = ET.fromstring( s )
-        if tree is None:  return None
-        return self.parse_tree( tree )
+    def parse_record( rec: ET ) -> MARC.record:
+        mrec = MARC.record()
 
-    #-------------------------------------------------------------------
-    # Open the named file and attempt to parse its contents as XML.
-    # Returns an ElementTruee on success and None on failure.
-    #
-    def parse_file( self, path ):
-        tree = ET.parse( path )
-        if tree is None: return None
-        return self.parse_tree( tree.getroot() )
-
-
-
-    #-------------------------------------------------------------------
-    # Return a list of MARC record objects contained in the
-    # ElementTree.  Ostensibly this should be only one, but LC queries
-    # can return multiple hits.  Returns empty list if the file
-    # contains no MARC records.
-    #
-    def parse_tree( self, root ):
-        records = []
-        
-        if root is None: return records
-        
-        reclist = root.find( tag('records') )
-        if reclist is None: return records
-
-        for rec in reclist:
-            records.append( self.parse_record( rec ) )
-            
-        return records
-
-
-    #-------------------------------------------------------------------
-    # Parse a single XML element representing a MARC21 record and
-    # return a new MARC_record object embodying it.  Returns None on
-    # error.
-    #
-    def parse_record( self, rec ):
-        mrec = MARC_bib()
-
-        # Not really accustomed to the MARC21 namespace prefixes, but
-        # this is how it seems to be.
-        #
+        # Not really accustomed to the MARC21 namespace prefixes, but this is how it seems to be.
         data = rec.find( tag('recordData') ).find( ns('record') )
         if data is None: return mrec
 
@@ -105,8 +54,7 @@ class MARC_XML_parser( object ):
         if data_fields is not None:
             for f in data_fields:
 
-                # Initialize or increment the sequence count for this
-                # tag.
+                # Initialize or increment the sequence count for this tag.
                 #
                 tag_name = f.attrib[ 'tag' ]
                 try:
@@ -115,7 +63,7 @@ class MARC_XML_parser( object ):
                     mrec.seqs[ tag_name ] = 1
 
                 # Tag, sequence, and indicators.
-                mtag = MARC_tag( tag = tag_name,
+                mtag = MARC.tag( tag = tag_name,
                                  ind = f.attrib['ind1'] + f.attrib['ind2'],
                                  seq = mrec.seqs[ tag_name ] )
 
@@ -124,7 +72,34 @@ class MARC_XML_parser( object ):
                     mtag.fields.append( sf.attrib['code'], sf.text )
 
                 mrec.tags.append( mtag )
-                
-        return mrec
-        
 
+        return mrec
+
+    reclist = root.find( tag('records') )
+    if reclist is None: return records
+
+    for rec in reclist:
+        records.append( parse_record( rec ) )
+
+    return records
+
+
+# @todo should probably figure out how to merge with the file-based function, since the distinction
+# for this purpose is silly.
+#
+def parse_str( s: str ) -> list:
+    """Parse <s> as XML and return the associated ElementTree, or None on failure."""
+
+    tree = ET.fromstring( s )
+    if tree is None:  return None
+    return parse_tree( tree )
+
+
+def parse_file( path: str ) -> list:
+    """Open the named file and attempt to parse its contents as XML. Returns an ElementTruee on success
+    and None on failure.
+
+    """
+    tree = ET.parse( path )
+    if tree is None: return None
+    return parse_tree( tree.getroot() )
