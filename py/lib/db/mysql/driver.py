96811c55467d8384b759ac7d9976c4365825553c
Jay Windley <jwindley@xmission.com>
2019-10-29 15:22:34 -0600

    Refactor low-level MySQL

--- py/lib/db/mysql/driver.py
+++ py/lib/db/mysql/driver.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -----------------------------------------------------------------------
 #  File:              TAG( data_bib.py )
 #  Description:       Database driver for MySQL
@@ -7,37 +7,44 @@
 #  Copyright:         (c) 2013 Jay Windley
 #                     All rights reserved.
 # -----------------------------------------------------------------------
+"""Low-lovel MySQL driver"""
 
 import os
 import sys
 import string
 import MySQLdb as mdb
 
-#
-# TODO:  Exceptions
-#
+def esc( self, s: str ) -> str:
+    """Escape the string for use as a single-quoted value in an INSERT statement."""
+    if isinstance( s, str ):
+        return s.replace( "'", "\\'" )
+    else:
+        return s
 
-########################################################################
-#
-#  Concrete low-level database driver that simply executes queries and
-#  returns results.
-#
-class mysql_driver( object ):
 
-    def __init__( self, conn_data ):
-        self.db = mdb.connect( conn_data['host'],
-                               conn_data['user'],
-                               conn_data['pass'],
-                               conn_data['db'],
-                               charset = 'utf8',
-                               use_unicode = True )
+class base( object ):
 
-    #-------------------------------------------------------------------
-    # Return a single value that is the single row and single column
-    # resulting from the given query. Returns None if the query
-    # produces no value.
-    #
-    def single_value( self, query ):
+    def __init__( self, conn_data: dict ):
+        self.db = mdb.connect(
+            conn_data['host'],
+            conn_data['user'],
+            conn_data['pass'],
+            conn_data['db'],
+            charset = 'utf8',
+            use_unicode = True )
+
+
+class reader( base ):
+    """Read-only access to database"""
+
+    def __init__( self, conn_data: dict ):
+        super( reader, self ).__init__( **kwargs )
+
+    def single_value( self, query: str ):
+        """Return a single value that is the single row and single column resulting from the given
+        query. Returns None if the query produces no value.
+
+        """
         cur = self.db.cursor()
         cur.execute( query )
         data = cur.fetchone()
@@ -45,23 +52,23 @@ class mysql_driver( object ):
         return data[0]
 
 
-    #-------------------------------------------------------------------
-    # Return an array of database rows that result from the given
-    # query.  Queries that return no rows return an empty list.
-    #
-    def row_array( self, query ):
+    def row_array( self, query: str ) -> list:
+        """Return a list of database rows that result from the given query.  Queries that return no rows
+        return an empty list.
+
+        """
         cur = self.db.cursor()
         cur.execute( query )
         data = cur.fetchall()
         return data
 
 
-    #-------------------------------------------------------------------
-    # Return an array of dictionaries that result from the given query,
-    # where the keys of each dictionary are the column names in the
-    # result and the values are the corresponding retrieve values.
-    #
-    def row_dict( self, query ):
+    def row_dict( self, query: str ) -> list:
+        """Return an array of dictionaries that result from the given query, where the keys of each
+        dictionary are the column names in the result and the values are the corresponding retrieve
+        values.
+
+        """
         cur = self.db.cursor()
         cur.execute( query )
         data = cur.fetchall()
@@ -72,11 +79,8 @@ class mysql_driver( object ):
         return results
 
 
-    #-------------------------------------------------------------------
-    # Return a row in dict form from a table having a designated ID
-    # field, by the ID.
-    #
-    def get_dict_by_ID( self, table, ID ):
+    def get_dict_by_ID( self, table: str, ID: str ) -> dict:
+        """Return a row in dict form from a table having a designated ID field, by the ID."""
         query = "SELECT * FROM {table} WHERE `ID` = '{ID}';".format(
             table = table,
             ID = ID
@@ -84,12 +88,28 @@ class mysql_driver( object ):
         return self.row_dict( query )[ 0 ]
 
 
-    #-------------------------------------------------------------------
-    # Add a dictionary to the given table where the dictionary indices
-    # are column names and the dictionary values are intended row
-    # contents.
-    #
-    def add_dict( self, table, dict ):
+    def execute( self, query: str ):
+        """Execute a query.  Results are stored in the cursor object associated with the connection
+        object.
+
+        """
+        cur = self.db.cursor()
+        cur.execute( query )
+
+
+
+
+class reader_writer( reader ):
+    """Read-write access to database"""
+
+    def __init__( self, conn_data: dict ):
+        super( reader_writer, self ).__init__( **kwargs )
+
+    def add_dict( self, table: str, dict: dict ):
+        """Add a dictionary to the given table where the dictionary indices are column names and the
+        dictionary values are intended row contents.
+
+        """
         indices = dict.keys()
         values = dict.values()
         self.execute(
@@ -104,11 +124,11 @@ class mysql_driver( object ):
         self.commit()
 
 
-    #-------------------------------------------------------------------
-    # Update a row in the table according to the given dictionary, whose
-    # keys are the column names and key is the primary key column name.
-    #
-    def update_dict( self, table, dict, key ):
+    def update_dict( self, table: str, dict: dict, key: str ):
+        """Update a row in the table according to the given dictionary, whose keys are the column names and
+        key is the primary key column name.
+
+        """
         indices = dict.keys()
         self.execute(
             "UPDATE {table} SET {assigns} WHERE `{key}` = '{val}';".format(
@@ -124,29 +144,6 @@ class mysql_driver( object ):
         self.commit()
 
 
-
-    #-------------------------------------------------------------------
-    # Execute a query.  Results are stored in the cursor object
-    # associated with the connection object.
-    #
-    def execute( self, query ):
-        cur = self.db.cursor()
-        cur.execute( query )
-
-
-    #-------------------------------------------------------------------
-    # Commit any pending transactions.
-    #
     def commit( self ):
+        """Commit any pending transactions."""
         self.db.commit()
-
-
-    # ------------------------------------------------------------------
-    # Escape the string for use as a single-quoted value in an INSERT
-    # statement.
-    #
-    def esc( self, s ):
-        if isinstance( s, str ):
-            return s.replace( "'", "\\'" )
-        else:
-            return s
