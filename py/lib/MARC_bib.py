df5e192a06db2541925ce361b9f6516f62ff87b6
Jay Windley <jwindley@xmission.com>
2019-10-28 18:20:05 -0600

    documentation and namespacae changes

--- py/lib/MARC_bib.py
+++ py/lib/MARC_bib.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-from MARC import MARC_record, MARC_fieldlist
+from MARC import record, fieldlist
 
-class MARC_bib( MARC_record ):
+class bib( MARC.record ):
 
     # Field list specifications for high-level accessors.  First three
     # characters are the tag number.  Rest of the string is field
@@ -13,7 +13,7 @@ class MARC_bib( MARC_record ):
     tl_title   = [ '240adkmnpr', '243admnpr', '245agknp' ]
     tl_imprint = [ '260bcg' ]
 
-    
+
     #----------------------------------------------------------------
     # Convience accessors and formatters.
     #
@@ -22,7 +22,7 @@ class MARC_bib( MARC_record ):
     #        defined over them.
     #
     def LC_call( self ):    return self.find( '050' ).join('')
-    def Dewey_call( self ): return self.find( '082' ).join( '--dc', fields = ['a', '2'] ) 
+    def Dewey_call( self ): return self.find( '082' ).join( '--dc', fields = ['a', '2'] )
     def ISBN( self ):       return self.find( '020' ).join( fields = ['a'] ).split()[0]
 
     def author( self ):     return self.tl_deref( self.tl_author  )
