191bcafd34bd0466af0efe61198ac87ae22ae2d5
Jay Windley <jwindley@xmission.com>
2019-10-28 21:16:07 -0600

    Comments

--- py/lib/IND.py
+++ py/lib/IND.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python3
-"""
+#-------------------------------------------------------------------------------
 #  File:              IND.py
 #  Description:       Base class for ID-Name-Description objects
 #  Author:            Jay Windley <jwindley>
 #  Created:           Fri Apr 17 23:25:32 2015
 #  Copyright:         (c) 2015 Jay Windley
 #                     All rights reserved.
-"""
+#-------------------------------------------------------------------------------
 
-fromk kwargable import kwargable
+from kwargable import kwargable
 
 
 class IND( kwargable ):
-    """
-    Base class for any of several objects that consist of (or include) an ID, a name, and a
+    """Base class for any of several objects that consist of (or include) an ID, a name, and a
     description.
 
     N.B., it is rather important that all subclass constructors explicitly call the super class
@@ -23,9 +22,9 @@ class IND( kwargable ):
 
     def __init__( self, **kwargs ):
 
-        """
-        kwargable constructor pattern.  Set the defaults for the canonical fields and then override
-        them as needed from the constructor arguments.
+        """kwargable constructor pattern.  Set the defaults for the canonical fields and then override them
+        as needed from the constructor arguments.
+
         """
         self.ID           = None
         self.name         = ''
