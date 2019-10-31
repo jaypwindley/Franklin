3df0dd6224f54f7fbd6326138d51e9dc01ad89ab
Jay Windley <jwindley@xmission.com>
2019-10-31 12:17:47 -0600

    Normalize to PYTHONPATH

--- py/lib/MARC/Z392.py
+++ py/lib/MARC/Z392.py
@@ -20,7 +20,7 @@
 """
 
 import base64
-import MARC
+from MARC import MARC
 
 FT = '\x1E'        # field terminator   : after each tag data sequence
 US = '\x1F'        # unit separator     : between tag subfields
